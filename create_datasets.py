7cd49db82857f26cb9d25806789bce74cb332880
Kinley Rabgay <80943717+kinley2-glitch@users.noreply.github.com>
2022-09-11 16:22:19 +0600

    Commit

---
+++ create_datasets.py
@@ -0,0 +1,9 @@
+import cv2
+import time
+import sys, os, glob
+
+sys.path.insert(1, 'possnet')
+sys.path.insert(1, 'face_landmark')
+sys.path.insert(1, 'hand_landmark')
+sys.path.insert(1, 'classifier')
+
